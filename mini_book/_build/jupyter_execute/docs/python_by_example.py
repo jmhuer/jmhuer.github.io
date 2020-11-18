433d67ee5d97071cbbe6a514b39bdb558cb26693
Juan M Huerta <jmhuer@gmail.com>
2020-11-17 22:52:53 -0500

    removed resume from index page

--- mini_book/_build/jupyter_execute/docs/python_by_example.py
+++ mini_book/_build/jupyter_execute/docs/python_by_example.py
@@ -2,7 +2,29 @@
 # coding: utf-8
 
 # # Music
+# ## Problem 1
 # 
-# ## Overview
+# Compute the eigenvalues and eigenvectors of the matrix
 # 
-# Under development ....
+# $
+# A_1 = \left[ \begin{array}{cc} 1 & 0 \\ 0 & 2 \end{array} \right]
+# $
+# 
+# and show that the eigenvectors are orthogonal to each other.
+# Compute the eigenvalues and eigenvectors of the matrix
+# 
+# $
+# A_2 = \left[ \begin{array}{cc} 1 & 1 \\ 0 & 2 \end{array} \right]
+# $
+# 
+# and show that the eigenvectors are not orthogonal to each other.
+# 
+# Compute the eigenvalues and eigenvectors of the matrix
+# 
+# $
+# A_3 = \left[ \begin{array}{cccc} 4 & 11 & 11 & 12 \\ 11 & 13 & 14 & 10 \\ 11 & 14 & 16 & 16 \\ 12 & 12 & 16 & 18 \end{array} \right].
+# $
+# 
+# Is this matrix positive semidefinite? Are the eigenvectors orthogonal to each other?
+# 
+# Repeat this with random matrices of your choosing. Note that symmetric matrices, whether positive definite or note (i.e., whether or not they have nonnegative eigenvalues) will always have eigenvectors that are perpendicular (orthogonal) to each other.
