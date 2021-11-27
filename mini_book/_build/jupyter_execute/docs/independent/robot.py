26fdf500c1ded905f10aba297e12e8bc46d24923
jmhuer <jmhuer@gmail.com>
2021-11-26 20:19:15 -0600

    fix

--- mini_book/_build/jupyter_execute/docs/independent/robot.py
+++ mini_book/_build/jupyter_execute/docs/independent/robot.py
@@ -5,7 +5,7 @@
 # 
 #  <sub> [← Back to Porfolio](https://jmhuer.github.io/mini_book/_build/html/docs/portfolio.html)</sub>
 # 
-# <img src="../../../../images/aelstm.png" align="center"/>
+# <img src="../../../../images/robotautonomy.png" align="center"/>
 # 
 # <br>
 # 
