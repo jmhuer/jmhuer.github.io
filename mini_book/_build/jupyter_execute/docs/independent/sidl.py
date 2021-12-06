54e02b9335e24e303d04cbf0acaf7a676253ef95
jmhuer <jmhuer@gmail.com>
2021-12-06 17:06:48 -0600

    .

--- mini_book/_build/jupyter_execute/docs/independent/sidl.py
+++ mini_book/_build/jupyter_execute/docs/independent/sidl.py
@@ -11,8 +11,6 @@
 # 
 # The temporal structure of music is full of shift-invariant patterns (e.g. motifs, ostinatos, loops, etc.). We propose using a Tempo- ral Convolutional Winner-Take-All (CONV- WTA) autoencoder to find a shift-invariant dictionary to represent symbolic, multivariate, musical signals. The model learns to represent fixed length drum beats and variable length pi- ano music. We discuss applications of this sparse representation such as de-noising musi- cal ideas, unsupervised learning of composer styles, and music generation. To assist in re- lated work we include interactive code along with the trained models
 # 
-# <br>
-# 
 # 
 # ## Introduction
 # ---
@@ -30,6 +28,7 @@
 # 
 # 
 # ## Problem Defenition
+# ---
 # 
 # It is clear automatically varying the complexity of music has valuable applications. But how do we approach this problem without supervision? In words, we want to add or remove notes without diverging too much from the original "feeling" of music. In math we write:
 # > **Shift Invariant Dictionary Learning**:
@@ -43,13 +42,62 @@
 # 
 # 
 # ## How do we solve this problem?
+# ---
 # 
 # In order to reconstruct pitch vectors with the extra criteria of maintaining theoriginal chord/harmonic functionality, we propose a combined loss of MSE onpitch vector reconstruction and Cross Entropy on symbolic chord targets. Thereconstruction should be such that we do not lose the ability to map the originalchords but the information bottleneck serves to generalize pitch functionality
 # 
 # 
 # ## Results
+# ---
 # 
-# Below, are some of the results. To verify we have a good representation we can plot the representation of the data using our dictionary representation
+# ### Visualization
+# Below, are some of the results. To verify we have a good representation we can plot the top-2 components our encoded data
 # 
 # 
 # <img src="../../../../images/pca.png" align="center"/>
+# 
+# 
+# ### Interpolating Drum Data
+# 
+# One way of generating new music is to interpolate latent space. There are various ways of doing this, but in essence you can think about a convex convination between n examples. For our example we simply average sparse code between two examples:
+# 
+# 
+# **Original 1**
+# 
+# <audio controls>
+#   <source src="../../../../audio/target.wav" type="audio/wav">
+# Your browser does not support the audio element.
+# </audio><br>
+# <br>
+# 
+# **Original 2**
+# 
+# <audio controls>
+#   <source src="../../../../audio/a.wav" type="audio/wav">
+# Your browser does not support the audio element.
+# </audio><br>
+# <br>
+# 
+# **Interpolated Result**
+# 
+# <audio controls>
+#   <source src="../../../../audio/a.wav" type="audio/wav">
+# Your browser does not support the audio element.
+# </audio><br>
+# <br>
+# 
+# 
+# 
+# 
+# ### Average Dictionary Activity
+# 
+# Finally another useful application for discovering style of a symbolic piece of music in an unsupervised way. To do this we average dictionary activity of arbitrary length music and plot main two compoenents:
+# 
+# <img src="../../../../images/sidl.png" align="center"/>
+# 
+# 
+# ## Conclusion
+# ---
+# 
+# We have shown that a Temporal CONV-WTA Autoencoder can learn a sparse representation of arbitrary length symbolic musical signal. This shift-invariant, sparse representation can be used to analyze features, de-noise, extract style, and to generate musical content in a structured or unstructured way. The reconstruction and generation for the drum (Groove) dataset was significantly better than the piano (MAESTRO) dataset. This is in part because the drum dataset was preprocessed to match with the kernel size, all drum sections were the same length, and had lower dimensionality in comparison. \\
+# In the future, we hope to use a larger and more diverse dataset, improve reconstruction performance, and apply similar preprocessing to the piano data as done for the drum data. We also plan to further develop applications of this technology and build tools for artists.
